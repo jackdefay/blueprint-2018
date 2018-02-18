326a5cf243404516e8566b5bbad73d092fcb9de6
manavkulshrestha <manavkoolz@gmail.com>
2018-02-18 12:19:25 -0500

    Wrote out Comm class

--- mainReceiverCode/src/main.cpp
+++ mainReceiverCode/src/main.cpp
@@ -2,69 +2,56 @@
 #include <Arduino.h>
 #include <SPI.h>
 #include <RH_RF69.h>
+#include <Comm.h>
+// #include "../common/Defs.h"
 
-#define RF69_FREQUENCY  900.0
-#define RFM69_SLAVE     8
-#define RFM69_INTERRUPT 3
-#define RFM69_RST       4
+//defs for radio
+// #define RF69_FREQUENCY  900.0
+// #define RFM69_SLAVE     8
+// #define RFM69_INTERRUPT 3
+// #define RFM69_RST       4
 
 RH_RF69 rf69(RFM69_SLAVE, RFM69_INTERRUPT);
+Comm rad(rf69);
 
 void setup()
 {
   Serial.begin(115200);
-  while (!Serial);
-
-  pinMode(RFM69_RST, OUTPUT);
-  digitalWrite(RFM69_RST, LOW);
-
-  // manual reset
-  digitalWrite(RFM69_RST, HIGH);
-  delay(10);
-  digitalWrite(RFM69_RST, LOW);
-  delay(10);
-
-  if (!rf69.init())
-    Serial.println("init failed");
-
-    Serial.println("Initialized");
-
-  if (!rf69.setFrequency(RF69_FREQUENCY))
-    Serial.println("setFrequency failed");
-
-  uint8_t key[] = { 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08,
-                    0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08};
-  rf69.setEncryptionKey(key);
+  while (!Serial)
+    ;
 }
 
 void loop()
 {
-  Serial.println("Sending to rf69_server");
+  // Serial.println("Sending to rf69_server");
   // Send a message to rf69_server
   // uint8_t data[] = "12:25";
   // rf69.send(data, sizeof(data));
   //
   // rf69.waitPacketSent();
   // Now wait for a reply
-  uint8_t buf[RH_RF69_MAX_MESSAGE_LEN];
-  uint8_t len = sizeof(buf);
+  // uint8_t buf[RH_RF69_MAX_MESSAGE_LEN];
+  // uint8_t len = sizeof(buf);
+  //
+  // if (rf69.waitAvailableTimeout(500))
+  // {
+  //   // Should be a reply message for us now
+  //   if (rf69.recv(buf, &len))
+  //   {
+  //     Serial.print("got reply: ");
+  //     Serial.println((char*)buf);
+  //   }
+  //   else
+  //   {
+  //     Serial.println("recv failed");
+  //   }
+  // }
+  // else
+  // {
+  //   Serial.println("No reply, is rf69_server running?");
+  // }
 
-  if (rf69.waitAvailableTimeout(500))
-  {
-    // Should be a reply message for us now
-    if (rf69.recv(buf, &len))
-    {
-      Serial.print("got reply: ");
-      Serial.println((char*)buf);
-    }
-    else
-    {
-      Serial.println("recv failed");
-    }
-  }
-  else
-  {
-    Serial.println("No reply, is rf69_server running?");
-  }
+  rad.send(rf69, "test");
+
   delay(400);
 }
--- mainSenderCode/src/main.cpp
+++ mainSenderCode/src/main.cpp
@@ -2,6 +2,14 @@
 #include <Adafruit_seesaw.h>
 #include <SPI.h>
 #include <RH_RF69.h>
+#include <Comm.h>
+// #include "../common/Defs.h"
+
+// //defs for radio
+// #define RF69_FREQUENCY  900.0
+// #define RFM69_SLAVE     8
+// #define RFM69_INTERRUPT 3
+// #define RFM69_RST       4
 
 //defs for controller
 Adafruit_seesaw ss;
@@ -13,13 +21,8 @@ Adafruit_seesaw ss;
 uint32_t button_mask = (1 << BUTTON_RIGHT) | (1 << BUTTON_DOWN) | (1 << BUTTON_LEFT) | (1 << BUTTON_UP) | (1 << BUTTON_SEL);
 #define IRQ_PIN   5
 
-//defs for radio
-#define RF69_FREQUENCY  900.0
-#define RFM69_SLAVE     8
-#define RFM69_INTERRUPT 3
-#define RFM69_RST       4
-
 RH_RF69 rf69(RFM69_SLAVE, RFM69_INTERRUPT);
+Comm rad(rf69);
 
 void setup() {
   Serial.begin(115200);
@@ -29,27 +32,6 @@ void setup() {
   ss.pinModeBulk(button_mask, INPUT_PULLUP);
   ss.setGPIOInterrupts(button_mask, 1);
   pinMode(IRQ_PIN, INPUT);
-
-  //radio setup
-  pinMode(RFM69_RST, OUTPUT);
-  digitalWrite(RFM69_RST, LOW);
-
-  //manual reset
-  digitalWrite(RFM69_RST, HIGH);
-  delay(10);
-  digitalWrite(RFM69_RST, LOW);
-  delay(10);
-
-  if (!rf69.init()) {
-    while (1);
-  }
-  Serial.println("RFM69 initialized");
-  if (!rf69.setFrequency(RF69_FREQUENCY))
-    Serial.println("setFrequency failed");
-
-  uint8_t key[] = { 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08,
-                    0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08};
-  rf69.setEncryptionKey(key);
 }
 
 void loop() {
@@ -59,15 +41,18 @@ void loop() {
   // int x = ss.analogRead(2);
   // int y = ss.analogRead(3);
 
-  Serial.println("Sending to rf69_server");
-  // Send a message to rf69_server
-  uint8_t data[] = "12:25";
-  rf69.send(data, sizeof(data));
+  // Serial.println("Sending to rf69_server");
+  // // Send a message to rf69_server
+  // uint8_t data[] = "12:25";
+  // rf69.send(data, sizeof(data));
+  //
+  // rf69.waitPacketSent();
+  // Serial.println("SENT!");
+  //
+  // delay(400);
+  //
+  // delay(10);
 
-  rf69.waitPacketSent();
-  Serial.println("SENT!");
-
+  rad.receive(rf69, 500);
   delay(400);
-
-  delay(10);
 }
