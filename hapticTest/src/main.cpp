f6a7f48808f2f54aab6063e33183adea6a61a32d
thebeninator1 <thebeninator1@gmail.com>
2018-02-18 14:25:52 -0500

    Moved my code to the right spot

--- hapticTest/src/main.cpp
+++ hapticTest/src/main.cpp
@@ -1,35 +1,11 @@
 #include <Arduino.h>
 
-
-const int sendPin = 10;
-const int receivePin = 11;
-
 void setup() {
-    pinMode(sendPin, OUTPUT);
-    pinMode(receivePin, LOW);
-    Serial.begin(115200);
+
 }
 
 void loop() {
-    unsigned long sendTime;
-    unsigned long receiveTime;
-    unsigned long timeBetween;
-    unsigned long distanceCm;
 
-    sendTime = micros();
-    digitalWrite(sendPin, HIGH);
-    delayMicroseconds(10);
-    digitalWrite(sendPin, LOW);
-
-    while(digitalRead(receivePin) == 0);
-    while(digitalRead(receivePin) == 1);
-
-    receiveTime = micros();
-    timeBetween = sendTime-receiveTime;
-
-    distanceCm = timeBetween*17/1000%1000;
-    Serial.println(distanceCm);
-    delay(100);
 }   //the measurements are weird, but basically, the
 //value of 948 or higher is close, the value of 918 is
 //a foot-a foot and a half and 900 is about 2 feet
--- ultrasonicTest/src/main.cpp
+++ ultrasonicTest/src/main.cpp
@@ -5,23 +5,31 @@ const int sendPin = 10;
 const int receivePin = 11;
 
 void setup() {
-    pinMode(sendPin, OUTPUT)
+    pinMode(sendPin, OUTPUT);
     pinMode(receivePin, LOW);
     Serial.begin(115200);
 }
 
 void loop() {
-    unsigned long sendTime = 0;
-    unsigned long receiveTime = 0;
-    unsigned long timeBetween = 0;
-    unsigned int distanceCm = 0;
+    unsigned long sendTime;
+    unsigned long receiveTime;
+    unsigned long timeBetween;
+    unsigned long distanceCm;
 
+    sendTime = micros();
     digitalWrite(sendPin, HIGH);
     delayMicroseconds(10);
     digitalWrite(sendPin, LOW);
 
     while(digitalRead(receivePin) == 0);
+    while(digitalRead(receivePin) == 1);
 
-    sendTime = micros();
+    receiveTime = micros();
+    timeBetween = sendTime-receiveTime;
 
-}
+    distanceCm = timeBetween*17/1000%1000;
+    Serial.println(distanceCm);
+    delay(100);
+}   //the measurements are weird, but basically, the
+//value of 948 or higher is close, the value of 918 is
+//a foot-a foot and a half and 900 is about 2 feet
