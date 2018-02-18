d42ea66aafaa7460d9f7efe18d097c5d0f49fb74
manavkulshrestha <manavkoolz@gmail.com>
2018-02-18 11:08:34 -0500

    Added seesaw stuff for sender

--- mainSenderCode/src/main.cpp
+++ mainSenderCode/src/main.cpp
@@ -1,7 +1,19 @@
 #include <Arduino.h>
+#include <Adafruit_seesaw.h>
 #include <SPI.h>
 #include <RH_RF69.h>
 
+//defs for controller
+Adafruit_seesaw ss;
+#define BUTTON_RIGHT 6
+#define BUTTON_DOWN  7
+#define BUTTON_LEFT  9
+#define BUTTON_UP    10
+#define BUTTON_SEL   14
+uint32_t button_mask = (1 << BUTTON_RIGHT) | (1 << BUTTON_DOWN) | (1 << BUTTON_LEFT) | (1 << BUTTON_UP) | (1 << BUTTON_SEL);
+#define IRQ_PIN   5
+
+//defs for radio
 #define RF69_FREQUENCY  900.0
 #define RFM69_SLAVE     8
 #define RFM69_INTERRUPT 3
@@ -9,25 +21,29 @@
 
 RH_RF69 rf69(RFM69_SLAVE, RFM69_INTERRUPT);
 
-void setup()
-{
+void setup() {
   Serial.begin(115200);
-  while (!Serial);
 
+  //controller setup
+  ss.begin(0x49);
+  ss.pinModeBulk(button_mask, INPUT_PULLUP);
+  ss.setGPIOInterrupts(button_mask, 1);
+  pinMode(IRQ_PIN, INPUT);
+
+  //radio setup
   pinMode(RFM69_RST, OUTPUT);
   digitalWrite(RFM69_RST, LOW);
 
-  // manual reset
+  //manual reset
   digitalWrite(RFM69_RST, HIGH);
   delay(10);
   digitalWrite(RFM69_RST, LOW);
   delay(10);
 
-  if (!rf69.init())
-    Serial.println("init failed");
-
-    Serial.println("Initialized");
-
+  if (!rf69.init()) {
+    while (1);
+  }
+  Serial.println("RFM69 initialized");
   if (!rf69.setFrequency(RF69_FREQUENCY))
     Serial.println("setFrequency failed");
 
@@ -36,8 +52,13 @@ void setup()
   rf69.setEncryptionKey(key);
 }
 
-void loop()
-{
+void loop() {
+  // char radiopacket[20];
+  // char temp[5];
+  // String tempWord = "####";
+  // int x = ss.analogRead(2);
+  // int y = ss.analogRead(3);
+
   Serial.println("Sending to rf69_server");
   // Send a message to rf69_server
   uint8_t data[] = "12:25";
@@ -45,26 +66,8 @@ void loop()
 
   rf69.waitPacketSent();
   Serial.println("SENT!");
-  // Now wait for a reply
-  // uint8_t buf[RH_RF69_MAX_MESSAGE_LEN];
-  // uint8_t len = sizeof(buf);
 
-  // if (rf69.waitAvailableTimeout(500))
-  // {
-  //   // Should be a reply message for us now
-  //   if (rf69.recv(buf, &len))
-  //   {
-  //     Serial.print("got reply: ");
-  //     Serial.println((char*)buf);
-  //   }
-  //   else
-  //   {
-  //     Serial.println("recv failed");
-  //   }
-  // }
-  // else
-  // {
-  //   Serial.println("No reply, is rf69_server running?");
-  // }
   delay(400);
+
+  delay(10);
 }
